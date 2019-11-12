1acb2c212cc07bd4e25816548a9c970053df09ac
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-12 01:11:40 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum= num1+num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+subs= num1 - num2
+return subs
 end
 
 def division(num1, num2)
-
+  div = num1/num2
+return div
 end
 
 def multiplication(num1, num2)
-
+mult = num1*num2
+return mult
 end
 
 def modulo(num1, num2)
+  mod =num1 % num2
 
 end
 
 def square_root(num)
-
+root = Math.sqrt(num)
+return root
 end
